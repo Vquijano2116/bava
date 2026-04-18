144ee341c969c1077fe11542145a917378770462
Vquijano2116 <v.quijano@gmail.com>
2026-04-17 23:53:15 -0500

    si

---
+++ app.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var config_1 = require("@tanstack/react-start/config");
+exports.default = (0, config_1.defineConfig)({
+    server: {
+        preset: 'vercel',
+    },
+});
